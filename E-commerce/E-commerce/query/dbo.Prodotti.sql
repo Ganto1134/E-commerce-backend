ef12e4a2dbd489fb17c00074d3a05ef3fc2723c8
rikisan96 <riccardocutruneo@gmail.com>
2024-07-11 10:51:46 +0200

    Added working View

--- E-commerce/E-commerce/query/dbo.Prodotti.sql
+++ E-commerce/E-commerce/query/dbo.Prodotti.sql
@@ -1,17 +1,4 @@
-﻿CREATE TABLE [dbo].[Prodotti] (
-    [IDProdotto]      INT             IDENTITY (1, 1) NOT NULL,
-    [Nome]            NVARCHAR (255)  NOT NULL,
-    [Descrizione]     NVARCHAR (255)  NULL,
-    [Prezzo]          DECIMAL (10, 2) NOT NULL,
-    [Categoria]       NVARCHAR (100)  NULL,
-    [DataInserimento] DATETIME        DEFAULT (getdate()) NULL,
-    [ImmagineLink]    NVARCHAR (255)  NULL,
-    PRIMARY KEY CLUSTERED ([IDProdotto] ASC)
-);
-
-
-
-
+﻿
 
 --popolamento tabella Prodotti
 INSERT INTO [dbo].[Prodotti] 
